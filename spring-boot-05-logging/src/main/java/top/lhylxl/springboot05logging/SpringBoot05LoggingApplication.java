30e8d5b8a389e6d5745daa91324af8f4e43c502b
MuGe <MuGe@qq.com>
2019-12-03 09:36:35 +0800

    日志框架第一次提交

---
+++ spring-boot-05-logging/src/main/java/top/lhylxl/springboot05logging/SpringBoot05LoggingApplication.java
@@ -0,0 +1,13 @@
+package top.lhylxl.springboot05logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot05LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot05LoggingApplication.class, args);
+	}
+
+}
---
+++ spring-boot-05-logging/src/test/java/top/lhylxl/springboot05logging/SpringBoot05LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lhylxl.springboot05logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot05LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
